aa0d27008f969e61b95cb789271c0c72727b4133
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2020-12-01 16:31:59 +0000

    Log parser with json payload

--- src/Logs/Parser.php
+++ src/Logs/Parser.php
@@ -2,6 +2,9 @@
 
 namespace Sculptor\Agent\Logs;
 
+use Illuminate\Support\Str;
+use Rap2hpoutre\LaravelLogViewer\LaravelLogViewer;
+
 /*
  * (c) Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
  *  For the full copyright and license information, please view the LICENSE
@@ -10,5 +13,37 @@ namespace Sculptor\Agent\Logs;
 
 class Parser
 {
+    public const CONTEXT_PATTERN = '~\{(?:[^{}]|(?R))*\}~';
 
+    /**
+     * @var LaravelLogViewer
+     */
+    private $parser;
+
+    public function __construct()
+    {
+        $this->parser = new LaravelLogViewer;
+    }
+
+    public function all(string $file): array
+    {
+        $this->parser->setFile($file);
+
+        $parsed =  $this->parser->all();
+
+        foreach ($parsed as &$line) {
+            if (preg_match(Parser::CONTEXT_PATTERN, $line['text'], $match) > 0) {
+                $line['text'] = Str::of($line['text'])->replace($match[0], '')->trim() . '';
+
+                $line['payload'] = $match;
+            }
+        }
+
+        return $parsed;
+    }
+    
+    public function files(): array
+    {
+        return $this->parser->getFolderFiles();
+    }
 }
